80091235291b0bcaa800e35dc387c9a9d224faa1
prfagun1 <prfagun1@hotmail.com>
2017-07-01 21:47:01 -0300

    Version 1.0

---
+++ PDFSignatureTool/Class/Methods.cs
@@ -0,0 +1,124 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.security;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFSignatureTool.Class{
+    class Methods   {
+
+  
+        private void sign(X509Certificate2 cert, String imput, String output){
+            string SourcePdfFileName = imput;
+            string DestPdfFileName = output;
+            string requerente = "";
+            Org.BouncyCastle.X509.X509CertificateParser cp = new Org.BouncyCastle.X509.X509CertificateParser();
+            Org.BouncyCastle.X509.X509Certificate[] chain = new Org.BouncyCastle.X509.X509Certificate[] { cp.ReadCertificate(cert.RawData) };
+            IExternalSignature externalSignature = new X509Certificate2Signature(cert, "SHA-1");
+            PdfReader pdfReader = new PdfReader(SourcePdfFileName);
+            FileStream signedPdf = new FileStream(DestPdfFileName, FileMode.Create);  //the output pdf file
+            PdfStamper pdfStamper = PdfStamper.CreateSignature(pdfReader, signedPdf, '\0');
+            PdfSignatureAppearance signatureAppearance = pdfStamper.SignatureAppearance;
+
+            requerente = cert.Subject.Replace("CN=", "").Replace("OU=", "").Replace("DC=", "").Replace("O=", "").Replace("C=", "");
+
+            signatureAppearance.SetVisibleSignature( new iTextSharp.text.Rectangle(250, 30, 550, 80),1,  "Signature");
+            signatureAppearance.Layer2Text = "Assinado de forma digital por " + requerente + Environment.NewLine + "Dados:" + DateTime.Now;
+
+            string pathImage = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "assinatura.png");
+            var image = iTextSharp.text.Image.GetInstance(pathImage);
+
+
+
+
+            signatureAppearance.SignatureGraphic = image;
+
+
+
+
+
+            //signatureAppearance.SignatureRenderingMode = PdfSignatureAppearance.RenderingMode.DESCRIPTION;
+            signatureAppearance.SignatureRenderingMode = PdfSignatureAppearance.RenderingMode.GRAPHIC_AND_DESCRIPTION;
+
+            //signatureAppearance.SignatureRenderingMode = PdfSignatureAppearance.RenderingMode.DESCRIPTION;
+            MakeSignature.SignDetached(signatureAppearance, externalSignature, chain, null, null, null, 0, CryptoStandard.CMS);
+        }
+
+
+        public String signPDF(String certName, String folder){
+            String result = "";
+            String output = folder + "\\Assinados\\";
+            DirectoryInfo dSaida = new DirectoryInfo(output);
+            dSaida.Create();
+
+            X509Certificate2 cert = this.getCert(certName);
+
+            DirectoryInfo d = new DirectoryInfo(folder);
+            FileInfo[] Files = d.GetFiles("*.pdf");
+
+            //Lista todos os arquivos PDF da pasta e executa o procedimento para cada um deles
+            foreach (FileInfo file in Files) {
+                try{
+                    this.sign(cert, file.FullName, output + file.Name);
+                    result += file.Name + " - Assinado" + Environment.NewLine;
+                }
+                catch(Exception erro){
+                    result += file.Name + " - Erro" + Environment.NewLine; ;
+                }
+            }
+
+            return result;
+        }
+
+
+        public X509Certificate2 getCert(String certName) { 
+            X509Certificate2Collection lcerts;
+            X509Store lStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+
+            // Abre o Store
+            lStore.Open(OpenFlags.ReadOnly);
+
+            // Lista os certificados
+            lcerts = lStore.Certificates;
+
+            foreach (X509Certificate2 cert in lcerts) {
+                if (certName == cert.Subject){
+                    return cert;
+                }
+            }
+
+            lStore.Close();
+            return null;
+        }
+
+
+        public List<String> getCertList() {
+            List<String> certList = new List<String>();
+
+            X509Certificate2Collection lcerts;
+            X509Store lStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+
+            // Abre o Store
+            lStore.Open(OpenFlags.ReadOnly);
+
+            // Lista os certificados
+            lcerts = lStore.Certificates;
+
+            foreach (X509Certificate2 cert in lcerts) {
+                if (cert.HasPrivateKey && cert.NotAfter > DateTime.Now && cert.NotBefore < DateTime.Now) {
+                    certList.Add(cert.Subject);
+                }
+            }
+            lStore.Close();
+            return certList;
+        }
+
+
+    }
+}
---
+++ PDFSignatureTool/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using PDFSignatureTool.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDFSignatureTool
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bAssinarDocumentos_Click(object sender, RoutedEventArgs e) {
+            Methods method = new Methods();
+            ComboboxItem item = (ComboboxItem)cCertificado.SelectedItem;
+            String result = "Local dos arquivos assinados: " + tPasta.Text + "\\Assinados" + Environment.NewLine + Environment.NewLine;
+
+            try {
+                result += method.signPDF(item.Value.ToString(), tPasta.Text);
+            }
+            catch{}
+
+            tRestuldado.Text = result;
+        }
+
+        private void bLocalizarPasta_Click(object sender, RoutedEventArgs e) {
+            FolderBrowserDialog navegarPasta = new FolderBrowserDialog();
+            DialogResult response = navegarPasta.ShowDialog();
+
+            if (response.ToString() == "OK") {
+                tPasta.Text = navegarPasta.SelectedPath;
+            }
+
+        }
+
+
+         private void loadCert(object sender, RoutedEventArgs e){
+            Methods method = new Methods();
+            foreach (String certificado in method.getCertList()) {
+                ComboboxItem item = new ComboboxItem();
+                item.Text = certificado.Replace("CN=", "").Replace("OU=", "").Replace("DC=", "").Replace("O=", "").Replace("C=", "");
+                item.Value = certificado;
+                cCertificado.Items.Add(item);
+            }
+        }
+    }
+
+    public class ComboboxItem {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public override string ToString() {
+            return Text;
+        }
+    }
+}
